3ab0b849f9d16d8d12f23a315cfc9ca95cfdbc21
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-21 13:43:36 +0400

    wrapped app with session provider/added user panel to Nav component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import Nav from "@/components/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { headers } from "next/headers";
+import AuthProvider from "@/components/AuthProvider";
+import { getActiveSession } from "@/utils/getSession";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,18 +12,22 @@ export const metadata = {
   description: "Discover & share AI-powered prompts",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getActiveSession(headers().get("cookie") ?? "");
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-h-screen">
-          <Nav />
-          <div className="container">{children}</div>
-        </main>
+        <AuthProvider session={session}>
+          <main className="min-h-screen">
+            <Nav />
+            <div className="container">{children}</div>
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
-import { CpuChipIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Nav = () => {
+  const { data: session } = useSession();
+  const [userPanelOpen, setUserPanelOpen] = useState(false);
+
   return (
     <nav className="nav">
       <div className="container nav-inner">
@@ -10,13 +16,40 @@ const Nav = () => {
             Prompts App
           </Link>
         </h1>
-        <Link
-          href="/prompt-create"
-          className="btn-default flex gap-2 items-center"
-        >
-          <CpuChipIcon width={16} />
-          Create Prompt
-        </Link>
+        <div className="user-panel-container">
+          {!session ? (
+            <button className="btn-default" onClick={() => signIn()}>
+              Sing In
+            </button>
+          ) : (
+            <>
+              <button
+                onClick={() => setUserPanelOpen(!userPanelOpen)}
+                className="text-black flex gap-2 items-center"
+              >
+                <img
+                  width={28}
+                  className="rounded-full"
+                  src={session.user?.image!}
+                />
+                <span className="text-sm">{session.user?.name}</span>
+              </button>
+              {userPanelOpen && (
+                <div className="user-panel">
+                  <Link className="btn-light" href={"/profile"}>
+                    Profile
+                  </Link>
+                  <Link className="btn-light" href={"/prompt-create"}>
+                    Create Prompt
+                  </Link>
+                  <button className="btn-dark" onClick={() => signOut()}>
+                    Sing Out
+                  </button>
+                </div>
+              )}
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );

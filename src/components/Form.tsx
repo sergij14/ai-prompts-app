258367f9a343262c8bd3268b052ebc9887c00f14
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-21 10:32:45 +0400

    created prompt form and form schema for validation

---
+++ src/components/Form.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Prompt, promptSchema } from "@/types";
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Prompt>({
+    resolver: yupResolver(promptSchema),
+  });
+
+  const onSubmit = async (data: Prompt) => {
+    try {
+      await fetch("/api/prompts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 items-center"
+    >
+      <div className="w-full max-w-md">
+        <label htmlFor="prompt" className="block mb-2 text-sm font-medium">
+          Prompt:
+        </label>
+
+        <textarea
+          className={`input w-full min-h-[120px] max-h-[200px]  ${
+            errors.tag && "border-red-500"
+          }`}
+          {...register("prompt")}
+        ></textarea>
+        <p className="text-red-500">{errors.prompt?.message}</p>
+      </div>
+
+      <div className="w-full max-w-md">
+        <label htmlFor="tag" className="block mb-2 text-sm font-medium">
+          Tag:
+        </label>
+        <input
+          className={`input w-full ${errors.tag && "border-red-500"}`}
+          {...register("tag")}
+        />
+        <p className="text-red-500">{errors.tag?.message}</p>
+      </div>
+
+      <button className="btn-default" type="submit">
+        Create
+      </button>
+    </form>
+  );
+};
+
+export default Form;
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
+import * as yup from "yup";
+
+export const promptSchema = yup
+  .object({
+    prompt: yup.string().required(),
+    tag: yup.string().required(),
+  })
+  .required();
+
 export type Prompt = {
   prompt: string;
   tag: string;

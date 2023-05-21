11381eab24c25b27ffa6a20df72bc7a59dd303a4
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-21 10:12:13 +0400

    updated Prompt model

--- src/models/Prompt.ts
+++ src/models/Prompt.ts
@@ -1,11 +1,6 @@
 import { Schema, model, models } from "mongoose";
 
 const PromptSchema = new Schema({
-  userDatabaseID: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "User ref is required."],
-  },
   prompt: {
     type: String,
     required: [true, "Prompt is required."],
@@ -14,16 +9,8 @@ const PromptSchema = new Schema({
     type: String,
     required: [true, "Tag is required."],
   },
-  creator: {
-    type: String,
-    required: [true, "Creator name is required."],
-  },
-  creatorImg: {
-    type: String,
-    required: [true, "Creator Image is required."],
-  },
 });
 
-const Prompt = models.Prompt || model("Prompt", PromptSchema);
+const PromptModel = models.Prompt || model("Prompt", PromptSchema);
 
-export default Prompt;
+export default PromptModel;

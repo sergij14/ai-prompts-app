fc65220bfce3f3961ee997846e1d92a44ce29665
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-21 14:12:34 +0400

    added new properties to prompt type

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,11 @@
 export type Prompt = {
   prompt: string;
   tag: string;
-  author: string;
 };
 
 export type PromptFromDB = Prompt & {
+  author: string;
+  authorImg: string;
   _id: string;
+  userDatabaseID: string;
 };

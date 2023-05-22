fecb6178dd2cb3bbd15013076a4c3f230fd1425a
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-22 11:05:04 +0400

    added handler for getting single prompt

---
+++ src/app/api/prompt/route.ts
@@ -0,0 +1,20 @@
+import PromptModel from "@/models/Prompt";
+import { connectToDB } from "@/utils/connectToDB";
+
+export const GET = async (req: Request) => {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+
+  try {
+    await connectToDB();
+    const prompt = await PromptModel.findById(id);
+
+    if (!prompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Error deleting prompt", { status: 500 });
+  }
+};
